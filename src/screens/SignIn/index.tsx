2f276dff6e966715927e18ba044d850c23a9ad12
Victor Hugo <pavictorhugo@gmail.com>
2021-07-02 17:08:11 -0300

    valida campos de login com yup

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -5,16 +5,42 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
+import * as Yup from "yup";
+
 import { useTheme } from "styled-components";
 
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { PasswordInput } from "../../components/PasswordInput";
 import { Container, Header, Title, Form, Subtitle, Footer } from "./styles";
+import { Alert } from "react-native";
 
 export function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um e-mail válido"),
+        password: Yup.string().required("A senha é obrigatória"),
+      });
+
+      await schema.validate({ email, password });
+
+      //Fazer Login
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        return Alert.alert("Opa", error.message);
+      }
+      Alert.alert(
+        "Erro na autenticação",
+        "Ocorreu um erro ao fazer login, verifique as credenciais"
+      );
+    }
+  }
 
   const theme = useTheme();
   return (
@@ -44,6 +70,7 @@ export function SignIn() {
               onChangeText={setEmail}
               value={email}
             />
+
             <PasswordInput
               iconName="lock"
               placeholder="Senha"
@@ -55,8 +82,8 @@ export function SignIn() {
           <Footer>
             <Button
               title="Login"
-              onPress={() => {}}
-              enabled={false}
+              onPress={handleSignIn}
+              enabled={true}
               loading={false}
             />
             <Button
